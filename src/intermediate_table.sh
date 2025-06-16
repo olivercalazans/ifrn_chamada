7f5b8f975a5c6be06d990864bef7d3af2b2b40e1
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-06-16 13:17:31 -0300

    Update intermediate_table.sh

--- src/intermediate_table.sh
+++ src/intermediate_table.sh
@@ -1,6 +1,17 @@
-LOAD DATA LOCAL INFILE '/caminho/PLANILHAO.CSV'
+#!/bin/bash
+
+read -p 'Número do container: ' ID
+read -sp 'Informe sua senha: ' SENHA
+echo ""
+read -p 'Caminho completo do CSV (ex: /home/usuario/PLANILHAO.csv): ' CAMINHO
+
+mysql -u"CONTAINER0$ID" -p"$SENHA" -h "192.168.102.100" "BD0$ID" --local-infile=1 <<EOF
+
+LOAD DATA LOCAL INFILE '$CAMINHO'
 INTO TABLE aulas_raw
 CHARACTER SET latin1
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
+
+EOF
